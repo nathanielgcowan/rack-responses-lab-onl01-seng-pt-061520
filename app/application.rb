93255a112631c57b11f70ce19082a66d874a1b1a
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-15 04:23:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,14 @@
 class Application
+
   def call(env)
-    resp = Rack:Response.new 
+    resp = Rack::Response.new
     now = Time.now
-    if now.hour >= 12
+    if now.hour >=12
       resp.write "Good Afternoon"
-    else 
+    else
       resp.write "Good Morning"
     end
     resp.finish
   end
+
 end
